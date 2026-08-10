f8068c2b8f6ef42590519b0cde81b382441cd6ee
Duy Kieu <kieuquangduy6@gmail.com>
2026-08-11 02:05:13 +0700

    feat(common): add general form API Response

--- src/main/java/com/duy/aicommerce/backend/common/dto/ApiResponse.java
+++ src/main/java/com/duy/aicommerce/backend/common/dto/ApiResponse.java
@@ -1,4 +1,16 @@
 package com.duy.aicommerce.backend.common.dto;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+    private boolean success;
+    public static <T> ApiResponse<T> success(T data, String message) {return new ApiResponse<>(message, data, true);}
+    public static <T> ApiResponse<T> error(String message) {return new ApiResponse<>(message,null, false);}
 }
