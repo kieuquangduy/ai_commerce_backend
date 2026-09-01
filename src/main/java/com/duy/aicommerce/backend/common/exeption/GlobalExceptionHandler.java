08bfcba549698790056346c2df25259ed7c67173
Duy Kieu <kieuquangduy6@gmail.com>
2026-09-01 18:21:31 +0700

    feat(auth): Handling Exception

--- src/main/java/com/duy/aicommerce/backend/common/exeption/GlobalExceptionHandler.java
+++ src/main/java/com/duy/aicommerce/backend/common/exeption/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.duy.aicommerce.backend.common.exeption;
 
+import com.duy.aicommerce.backend.auth.exception.InvalidRefreshTokenException;
+import com.duy.aicommerce.backend.auth.exception.InvalidVerificationToken;
 import com.duy.aicommerce.backend.common.dto.ApiResponse;
 import com.duy.aicommerce.backend.role.exception.RoleNotFoundException;
 import com.duy.aicommerce.backend.user.exception.EmailExistException;
+import com.duy.aicommerce.backend.user.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +13,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidRefreshTokenException.class)
+    public ResponseEntity<ApiResponse> handleInvalidRefreshTokenException(InvalidRefreshTokenException ex) {
+        return new ResponseEntity<>(ApiResponse.error(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(EmailExistException.class)
     public ResponseEntity<ApiResponse> handleEmailExistException(EmailExistException ex) {
@@ -19,5 +27,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RoleNotFoundException.class)
     public ResponseEntity<ApiResponse> handleRoleNotFoundException(RoleNotFoundException ex) {
         return  new ResponseEntity<>(ApiResponse.error(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidVerificationToken.class)
+    public ResponseEntity<ApiResponse> handleInvalidVerificationToken(InvalidVerificationToken ex) {
+        return new ResponseEntity<>(ApiResponse.error(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleUserNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ApiResponse.error(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
