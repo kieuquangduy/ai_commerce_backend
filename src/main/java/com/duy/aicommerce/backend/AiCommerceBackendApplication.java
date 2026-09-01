ff0b3be9c9c5a1b06b1445597283a86457b73ca1
Duy Kieu <kieuquangduy6@gmail.com>
2026-09-01 18:26:38 +0700

    feat(auth): adding ConfigurationPropertiesScan

--- src/main/java/com/duy/aicommerce/backend/AiCommerceBackendApplication.java
+++ src/main/java/com/duy/aicommerce/backend/AiCommerceBackendApplication.java
@@ -2,8 +2,11 @@ package com.duy.aicommerce.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AiCommerceBackendApplication {
 
     public static void main(String[] args) {
