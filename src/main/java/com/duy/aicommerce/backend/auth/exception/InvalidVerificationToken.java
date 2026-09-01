03a315d50a199dcd79f9a9f1bcbe291eac999896
Duy Kieu <kieuquangduy6@gmail.com>
2026-09-01 18:20:07 +0700

    feat(auth): Handling Token Module Exception

--- src/main/java/com/duy/aicommerce/backend/auth/exception/InvalidVerificationToken.java
+++ src/main/java/com/duy/aicommerce/backend/auth/exception/InvalidVerificationToken.java
@@ -1,7 +1,7 @@
 package com.duy.aicommerce.backend.auth.exception;
 
 public class InvalidVerificationToken extends RuntimeException {
-  public InvalidVerificationToken(String message) {
-    super(message);
-  }
+    public InvalidVerificationToken(String message) {
+        super(message);
+    }
 }
